a2951afef144fb95c86aa536f66184771db0d876
Tarx007 <168792847+Tarx007@users.noreply.github.com>
2026-04-07 22:02:25 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -84,7 +84,7 @@ async def close() -> Dict[str, str]:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
     uvicorn.run(
-        "server:app",
+        "server.app:app",
         host="0.0.0.0",
         port=port,
         reload=False,
